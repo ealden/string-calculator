4c6479991c232bb607849b5e6a7d850b8c811533
Ealden Esto E. Escañan <ealden@escanan.com>
2019-08-20 19:38:37 +0800

    Use single quote

--- string_calculator_test.rb
+++ string_calculator_test.rb
@@ -4,19 +4,19 @@ require_relative 'string_calculator'
 
 class StringCalculatorTest < Minitest::Test
   def test_return_zero_if_numbers_is_empty
-    assert_equal 0, add("")
+    assert_equal 0, add('')
   end
 
   def test_return_number_if_numbers_is_not_empty
-    assert_equal 1, add("1")
+    assert_equal 1, add('1')
   end
 
   def test_return_sum_if_numbers_has_one_comma
-    assert_equal 3, add("1,2")
+    assert_equal 3, add('1,2')
   end
 
   def test_return_sum_if_numbers_has_many_values
-    assert_equal 15, add("1,2,3,4,5")
+    assert_equal 15, add('1,2,3,4,5')
   end
 
   private
